b6c44b352ad296b731a78efb09036ee7e1f5569f
Justiket <caoyinglong@126.com>
2025-01-22 09:48:37 +0800

    init

---
+++ src/main/java/com/caoyinglong/transaction/HsbcHometaskTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.caoyinglong.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcHometaskTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsbcHometaskTransactionApplication.class, args);
+	}
+
+}
